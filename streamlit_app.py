e35d3c6e5cbe0e2a0f7fb0cb8f066b683fb501ce
pmchancrin <61418790+pmchancrin@users.noreply.github.com>
2024-02-06 08:48:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Hard-Boiled Free-Range egg')

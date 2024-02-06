7bd7cac92cd48cbc7a2142387e5b6fa25da07595
pmchancrin <61418790+pmchancrin@users.noreply.github.com>
2024-02-06 17:41:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets[“snowflake”])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute(“SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()”)
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text(“Hello from Snowflake:”)
 streamlit.text(my_data_row)

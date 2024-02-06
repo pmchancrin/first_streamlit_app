b67031f1310b07f5c385e0987bd4c3978f4cb026
pmchancrin <61418790+pmchancrin@users.noreply.github.com>
2024-02-06 08:47:16 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit # first_streamlit_app
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.test('Omega 3 & Blueberry Oatmeal')
-streamlit.test('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
